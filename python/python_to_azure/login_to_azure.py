3f3453d639a16e92d56b74d35f2c26d3242df9d4
unknown <lovemasta41@gmail.com>
2021-08-22 16:04:49 +0530

    code to login to azure commited

---
+++ python/python_to_azure/login_to_azure.py
@@ -0,0 +1,19 @@
+#required pip install azure-identity
+#required pip install azure-mgmt-resource
+#required pip install azure.mgmt.subscription
+#azure account extension
+from re import sub
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.subscription import SubscriptionClient
+from azure.mgmt.resource.resources import ResourceManagementClient
+
+credentials = DefaultAzureCredential()
+subs_client = SubscriptionClient(credential=credentials, baseurl=None, kwargs=any)
+
+for x in subs_client.subscriptions.list():
+    print(x.as_dict()['subscription_id'])
+    res_client = ResourceManagementClient(credential=credentials,subscription_id=x.as_dict()['subscription_id'])
+    for resource_group in res_client.resource_groups.list():
+        print(resource_group.as_dict()['name'])
+
+#print(f"successful credential: {credentials._successful_credential}")
